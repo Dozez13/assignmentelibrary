65a18410b0703e6da27b6429754c5c2a2bd6ceb2
Pavlo Manuilenko <manuilenkopavel@gmail.com>
2024-06-07 15:50:04 +0300

    Initial commit

---
+++ src/main/java/com/faceit/assignmentelibrary/AssignmentelibraryApplication.java
@@ -0,0 +1,13 @@
+package com.faceit.assignmentelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentelibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentelibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faceit/assignmentelibrary/AssignmentelibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faceit.assignmentelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentelibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
